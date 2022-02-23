7e756286aa53353d8c57239cefef0d5fb0186c35
Ken Zhou <74420508+notken12@users.noreply.github.com>
2022-02-23 16:00:24 +0000

    finish game

--- src/guess.rs
+++ src/guess.rs
@@ -1,4 +1,8 @@
 use crate::words::{Word, WordErr, WordLists};
+// Import (via `use`) the `fmt` module to make it available.
+use std::fmt;
+use ansi_term::Style;
+use ansi_term::Color::Fixed;
 
 pub struct Guess {
     pub word: Word,
@@ -23,6 +27,26 @@ impl Guess {
     }
 }
 
+impl fmt::Display for Guess {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+      let mut result = String::new();
+      for i in 0..self.word.0.len() {
+        let hint = &self.hints[i];
+        let bytes = vec![32, self.word.0.as_bytes()[i], 32];
+        let letter = std::str::from_utf8(&bytes).expect("");
+        let formatted = format!("{}", hint.style(letter));
+        result += &formatted;
+        result += &" ";
+      }
+
+    write!(f, "{}", result)
+    }
+}
+
 fn get_hints(word: &Word, answer: &String) -> [Hint; Word::LENGTH] {
     let mut hints: [Hint; Word::LENGTH] = [Hint::Wrong; Word::LENGTH];
     let word = &word.0;
@@ -33,15 +57,13 @@ fn get_hints(word: &Word, answer: &String) -> [Hint; Word::LENGTH] {
         // println!("{}", i);
         // First check the same index
         if !used[i] {
-            let at_same_index = answer.as_bytes()[i];
-            if at_same_index == word.as_bytes()[i] {
+            let at_same_index = answer.as_bytes()[i] as char;
+            if at_same_index == word.as_bytes()[i] as char {
                 hints[i] = Hint::Correct;
                 used[i] = true;
                 continue;
             }
         }
-
-        let mut letter_found = false;
 
         // Then check the same letter at other places
         // println!("{}", answer.len());
@@ -57,15 +79,9 @@ fn get_hints(word: &Word, answer: &String) -> [Hint; Word::LENGTH] {
             if at_index == letter {
                 hints[i] = Hint::Elsewhere;
                 used[j] = true;
-                letter_found = true;
                 break;
             }
         }
-
-        // if !letter_found {
-        //     // No match
-        //     hints[i] = Hint::Wrong;
-        // }
     }
 
     hints
@@ -81,4 +97,14 @@ pub enum Hint {
     Wrong,
     Elsewhere,
     Correct,
+}
+
+impl Hint {
+  pub fn style(&self, str: &str) -> String {
+    match &self {
+      Hint::Wrong => Style::new().on(Fixed(8)).bold().paint(str).to_string(),
+      Hint::Elsewhere => Style::new().on(Fixed(178)).bold().paint(str).to_string(),
+      Hint::Correct => Style::new().on(Fixed(46)).bold().paint(str).to_string()
+    }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,37 @@ mod words;
 use guess::{Guess, GuessErr};
 use words::{WordErr};
 use rand::Rng;
-use std::io;
+use std::io::{stdin,stdout,Write};
+
+use ansi_term::Style;
+use ansi_term::Color::Fixed;
 
 fn main() {
+  print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    // let enabled = ansi_term::enable_ansi_support();
+  
     let word_lists = words::get_word_lists();
-
+  
     let rand_index = rand::thread_rng().gen_range(0..word_lists.answer_words.len());
     let answer: &String = &word_lists.answer_words[rand_index];
 
-    println!("The answer is: {}", answer);
+  
+    println!("⬛🟨🟩 Wordle in Rust\n");
 
     let max_guesses = 6;
     let mut guesses = 0;
 
     while guesses < max_guesses {
+      
         let mut guess = String::new();
-        io::stdin()
+
+      print!("{}", Style::new().fg(Fixed(246)).paint("💭 Enter your guess: ").to_string());
+      let _=stdout().flush();
+        stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        guess.pop();
-        guess.pop();
+      
+        guess = guess.trim().to_string();
 
         let guess_result = Guess::new(guess, &word_lists, &answer);
 
@@ -36,10 +47,27 @@ fn main() {
                 GuessErr::NotInWordListErr => println!("Not in word list"),
             },
             Ok(guess) => {
-                guesses += 1;
-                println!("{:?}", guess.word);
-                println!("{:?}", guess.hints);
+              guesses += 1;
+              println!("{}", guess);
+
+              if guess.word.0.eq(answer) {
+                println!("\n🎉 Nice! You got it in {} {}.", guesses, if guesses == 1 {"try"} else {"tries"});
+          println!("\n🚥 Hit enter to play again or press Ctrl+C to quit.");
+                let mut s = String::new();
+                        stdin()
+            .read_line(&mut s)
+            .expect("Failed to read line");
+                main();
+              }
             }
         }
     }
+
+    println!("\n❌ Game over, the answer was {}", answer);
+          println!("\n🚥 Hit enter to play again or press Ctrl+C to quit.");
+        let mut s = String::new();
+                stdin()
+    .read_line(&mut s)
+    .expect("Failed to read line");
+  main();
 }
