91684729646f95ec74bfcbc22f1cdf4f8658b7ab
Ken Zhou <74420508+notken12@users.noreply.github.com>
2022-02-24 19:03:29 +0000

    check terminal support

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,21 @@ use ansi_term::Style;
 use ansi_term::Color::Fixed;
 
 fn main() {
+      match enable_ansi_support::enable_ansi_support() {
+        Ok(()) => {
+            // ANSI escape codes were successfully enabled, or this is a non-Windows platform.
+        }
+        Err(_) => {
+            // The operation was unsuccessful, typically because it's running on an older
+            // version of Windows. The program may choose to disable ANSI color code output in
+            // this case.
+          panic!("Your OS doesn't support colored terminal output.");
+        }
+    }
+    // Use your terminal color library of choice here.
+  // let enabled = ansi_term::enable_ansi_support();
   print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    // let enabled = ansi_term::enable_ansi_support();
-    
+  
     let rand_index = rand::thread_rng().gen_range(0..ANSWER_WORDS.len());
     let answer: &str = &ANSWER_WORDS[rand_index];
 
