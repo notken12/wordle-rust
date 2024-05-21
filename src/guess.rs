541440256993f8502ab2f5449f0e1a38ed8f3c79
Ken Zhou <kendotzhou@gmail.com>
2024-05-21 13:53:07 -0400

    fix hints

--- src/guess.rs
+++ src/guess.rs
@@ -3,8 +3,8 @@ use crate::words::{Word, WordErr};
 use ansi_term::Color::{Fixed, White};
 use ansi_term::Style;
 use std::fmt;
-use std::{thread, time::Duration};
 use std::io::{stdout, Write};
+use std::{thread, time::Duration};
 
 use crate::words::GUESS_WORDS;
 
@@ -29,26 +29,26 @@ impl Guess {
             Err(err) => Err(GuessErr::WordErr(err)),
         }
     }
-  pub fn display_hint(&self, i: usize) {
+    pub fn display_hint(&self, i: usize) {
         let word = self.word.0.to_uppercase();
         let mut result = String::new();
-                let hint = &self.hints[i];
-            let bytes = vec![32, word.as_bytes()[i], 32];
-            let letter = std::str::from_utf8(&bytes).expect("");
-            let formatted = format!("{}", hint.style(letter));
-            result += &formatted;
-            result += &" ";
-    print!("{}", result);
-      let _=stdout().flush();
-  }
-
-  pub fn display(&self) {
-    for i in 0..self.word.0.len() {
-      self.display_hint(i);
-      thread::sleep(Duration::from_millis(400));
+        let hint = &self.hints[i];
+        let bytes = vec![32, word.as_bytes()[i], 32];
+        let letter = std::str::from_utf8(&bytes).expect("");
+        let formatted = format!("{}", hint.style(letter));
+        result += &formatted;
+        result += &" ";
+        print!("{}", result);
+        let _ = stdout().flush();
     }
-    print!("\n");
-  }
+
+    pub fn display(&self) {
+        for i in 0..self.word.0.len() {
+            self.display_hint(i);
+            thread::sleep(Duration::from_millis(400));
+        }
+        print!("\n");
+    }
 }
 
 impl fmt::Display for Guess {
@@ -79,21 +79,25 @@ fn get_hints(word: &Word, answer: &str) -> [Hint; Word::LENGTH] {
     let answer = answer.clone();
     let mut used = [false; Word::LENGTH];
 
+    // Check for all letters that are in the right places
     for i in 0..answer.len() {
-      if !used[i] {
         let at_same_index = word.as_bytes()[i] as char;
-            if at_same_index == answer.as_bytes()[i] as char {
-                hints[i] = Hint::Correct;
-                used[i] = true;
-                continue;
-            }
-      }
+        if at_same_index == answer.as_bytes()[i] as char {
+            hints[i] = Hint::Correct;
+            used[i] = true;
+        }
+    }
 
-      // Then check the same letter at other places
-        // println!("{}", answer.len());
+    // Then check the same letter at other places
+    for i in 0..answer.len() {
+        if used[i] {
+            // skip over the letters that are in the right place
+            continue;
+        }
         for j in 0..word.len() {
             if used[j] {
                 // println!("continue because {} is used", i);
+                // skip over letters that already matched with answer
                 continue;
             }
 
@@ -126,9 +130,24 @@ pub enum Hint {
 impl Hint {
     pub fn style(&self, str: &str) -> String {
         match &self {
-            Hint::Wrong => Style::new().fg(White).on(Fixed(8)).bold().paint(str).to_string(),
-            Hint::Elsewhere => Style::new().fg(White).on(Fixed(178)).bold().paint(str).to_string(),
-            Hint::Correct => Style::new().fg(White).on(Fixed(70)).bold().paint(str).to_string(),
+            Hint::Wrong => Style::new()
+                .fg(White)
+                .on(Fixed(8))
+                .bold()
+                .paint(str)
+                .to_string(),
+            Hint::Elsewhere => Style::new()
+                .fg(White)
+                .on(Fixed(178))
+                .bold()
+                .paint(str)
+                .to_string(),
+            Hint::Correct => Style::new()
+                .fg(White)
+                .on(Fixed(70))
+                .bold()
+                .paint(str)
+                .to_string(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ mod guess;
 mod words;
 
 use guess::{Guess, GuessErr};
-use words::{WordErr, ANSWER_WORDS};
 use rand::Rng;
-use std::io::{stdin,stdout,Write};
+use std::io::{stdin, stdout, Write};
+use words::{WordErr, ANSWER_WORDS};
 
-use ansi_term::Style;
 use ansi_term::Color::Fixed;
+use ansi_term::Style;
 
 fn main() {
-      match enable_ansi_support::enable_ansi_support() {
+    match enable_ansi_support::enable_ansi_support() {
         Ok(()) => {
             // ANSI escape codes were successfully enabled, or this is a non-Windows platform.
         }
@@ -18,33 +18,43 @@ fn main() {
             // The operation was unsuccessful, typically because it's running on an older
             // version of Windows. The program may choose to disable ANSI color code output in
             // this case.
-          panic!("Your OS doesn't support colored terminal output.");
+            panic!("Your OS doesn't support colored terminal output.");
         }
     }
     // Use your terminal color library of choice here.
-  // let enabled = ansi_term::enable_ansi_support();
-  print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-  
+    // let enabled = ansi_term::enable_ansi_support();
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
     let rand_index = rand::thread_rng().gen_range(0..ANSWER_WORDS.len());
     let answer: &str = &ANSWER_WORDS[rand_index];
 
-  
-    println!("{} in {} 🦀" , Style::new().bold().fg(Fixed(0)).on(Fixed(15)).paint(" Wordle "), Style::new().bold().fg(Fixed(166)).paint("Rust"));
+    println!(
+        "{} in {} 🦀",
+        Style::new()
+            .bold()
+            .fg(Fixed(0))
+            .on(Fixed(15))
+            .paint(" Wordle "),
+        Style::new().bold().fg(Fixed(166)).paint("Rust")
+    );
     println!("{}\n", Style::new().fg(Fixed(248)).paint("by @notken12"));
 
     let max_guesses = 6;
     let mut guesses = 0;
 
     while guesses < max_guesses {
-      
         let mut guess = String::new();
 
-      print!("{}", Style::new().fg(Fixed(246)).paint("💭 Enter your guess: ").to_string());
-      let _=stdout().flush();
-        stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-      
+        print!(
+            "{}",
+            Style::new()
+                .fg(Fixed(246))
+                .paint("💭 Enter your guess: ")
+                .to_string()
+        );
+        let _ = stdout().flush();
+        stdin().read_line(&mut guess).expect("Failed to read line");
+
         guess = guess.trim().to_string().to_lowercase();
 
         let guess_result = Guess::new(guess, &answer);
@@ -58,27 +68,27 @@ fn main() {
                 GuessErr::NotInWordListErr => println!("Not in word list"),
             },
             Ok(guess) => {
-              guesses += 1;
-              guess.display();
+                guesses += 1;
+                guess.display();
 
-              if guess.word.0.eq(answer) {
-                println!("\n🎉 Nice! You got it in {} {}.", guesses, if guesses == 1 {"try"} else {"tries"});
-          println!("\n🚥 Hit enter to play again or press Ctrl+C to quit.");
-                let mut s = String::new();
-                        stdin()
-            .read_line(&mut s)
-            .expect("Failed to read line");
-                main();
-              }
+                if guess.word.0.eq(answer) {
+                    println!(
+                        "\n🎉 Nice! You got it in {} {}.",
+                        guesses,
+                        if guesses == 1 { "try" } else { "tries" }
+                    );
+                    println!("\n🚥 Hit enter to play again or press Ctrl+C to quit.");
+                    let mut s = String::new();
+                    stdin().read_line(&mut s).expect("Failed to read line");
+                    main();
+                }
             }
         }
     }
 
     println!("\n❌ Game over, the answer was {}", answer);
-          println!("\n🚥 Hit enter to play again or press Ctrl+C to quit.");
-        let mut s = String::new();
-                stdin()
-    .read_line(&mut s)
-    .expect("Failed to read line");
-  main();
+    println!("\n🚥 Hit enter to play again or press Ctrl+C to quit.");
+    let mut s = String::new();
+    stdin().read_line(&mut s).expect("Failed to read line");
+    main();
 }
